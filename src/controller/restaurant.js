bb1f9fe0e4cab9483b8d0b755b591a263ea69c14
Rahul N <rahul.n@ge.com>
2018-04-22 01:18:23 +0530

    updating data put requests in node

--- src/controller/restaurant.js
+++ src/controller/restaurant.js
@@ -29,11 +29,25 @@ export default({config, db}) => {
   });
 
   // GET 1 restaurant
-  // '/v1/restaurant/:id'
+  // '/v1/restaurant/:id' - Read 1
   api.get('/:id', (req, res) => {
     Restaurant.findById(req.params.id, (err, restaurants) => {
       if(err) res.send(err);
       res.json(restaurants);
+    });
+  });
+
+  // '/v1/restaurant/:id' - Update 1
+  api.put('/:id', (req, res) => {
+    Restaurant.findById(req.params.id, (err, restaurant) => {
+      if(err) res.send(err);
+      restaurant.name = req.body.name;
+      restaurant.save(err => {
+        if (err) res.send(err);
+        res.json(
+          {message: 'Restaurant updated successfully'}
+        );
+      });
     });
   });
 
